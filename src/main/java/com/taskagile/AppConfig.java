420f12af1710b72505214cb47c313c71cb3c7c08
jiyoung <jiyoung4061@naver.com>
2021-04-08 17:24:37 +0900

    first commit

---
+++ src/main/java/com/taskagile/AppConfig.java
@@ -0,0 +1,10 @@
+package com.taskagile;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.taskagile")
+public class AppConfig {
+  
+}
