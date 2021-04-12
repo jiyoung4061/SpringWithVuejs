114d8e1619d80bab784976890da071476701f013
jiyoung <jiyoung4061@naver.com>
2021-04-12 14:09:10 +0900

    2021.04.21 회원가입 폼 만들기

--- front-end/src/service/registration/__mocks__/index.js
+++ front-end/src/service/registration/__mocks__/index.js
@@ -1,6 +1,12 @@
 export default {
    register (detail) {
      return new Promise((resolve, reject) => {
+       /* Promise 객체
+          알수 없는 값에대한 대리자로, 비동기 연산이 종료된 후 결과값이나
+          실패이유를 처리하기위한 처리기
+          => 프로미스를 반환해 미래의 어떤 시점에 결과(resolve:이행 / reject:거절)를 제공한다.
+       */
+
        detail.emailAddress === 'sunny@local'
         ? resolve({result: 'success'})
         : reject(new Error('User already exist'))
---
+++ front-end/tests/unit/service.registration.spec.js
@@ -0,0 +1,49 @@
+import moxios from 'moxios'
+import registrationService from '@/service/registration'
+
+describe('service/registration', ()=>{
+  beforeEach(()=>{
+    moxios.install() // 목 생성
+    // 목 : test를 하기위해 만든 가짜 객체
+  })
+
+  afterEach(()=>{
+    moxios.uninstall() // 목 제거
+  })
+
+  it('should pass the response to caller when request succeeded', ()=>{
+    expect.assertions(2) // assertion이 호출되는 횟수 검증
+
+    // 목 요청이 만들어질 때까지 기다린다.
+    moxios.wait(()=>{
+      let request = moxios.requests.mostRecent() // 가장 최근 요청을 가져오고
+      expect(request).toBeTruthy() // 최근 요청이 존재했는지 검증
+      request.respondWith({ // 요청에 대한 응답 지정
+        status:200,
+        response:{result:'success'}
+      })
+    })
+
+    // register() 호출하고 그 결과로 반환된 값이 성공인지 확인
+    return registrationService.register().then(data => {
+      expect(data.result).toEqual('success')
+    })
+  })
+
+  it('should propagate the error to caller when request failed', ()=> {
+    expect.assertions(2)
+    moxios.wait(()=>{
+      let request = moxios.requests.mostRecent()
+      expect(request).toBeTruthy()
+      request.reject({
+        status: 400,
+        response: {message:'Bad Request'}
+      })
+    })
+
+    return registrationService.register().catch(error => {
+      expect(error.response.message).toEqual('Bad Request')
+    })
+  })
+  
+})
