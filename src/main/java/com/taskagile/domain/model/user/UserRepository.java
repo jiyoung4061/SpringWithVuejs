701685ab9a9cf5e04d643189de102faddd8b05b1
jiyoung <jiyoung4061@naver.com>
2021-04-14 10:20:36 +0900

    2021.04.14 9단원 마무리

--- src/main/java/com/taskagile/domain/model/user/UserRepository.java
+++ src/main/java/com/taskagile/domain/model/user/UserRepository.java
@@ -1,25 +1,21 @@
 package com.taskagile.domain.model.user;
 
 public interface UserRepository {
-
-  /**
-   * 
-   */
   User findByUsername(String username);
 
-  /**
-   * Find user by an email address
-   *
-   * @param emailAddress the provided email address used for finding user
-   * @return an instance of <code>User</code> if found, null otherwise
-   */
   User findByEmailAddress(String emailAddress);
 
-  /**
-   * Save a new user or an existing user
-   *
-   * @param user the user instance to be saved
-   * @return the newly save user
-   */
   void save(User user);
+  /*
+   * List<User> findByCreatedDateBetween(Date Date); List<User>
+   * findByLastNameOrderByFirstNameDesc(String lastName); //스프링 데이터 JPA 제공 기능 //
+   * 1. Pagination : 페이징 적용 List<User> findByLastName(String lastName, Pageable
+   * pageable); // 2. 메소드 이름으로 활용할수 있는 쿼리 키워드 리스트
+   *
+   * @Query("select u from User u where u.emailAddress = ?1") User
+   * findByEmailAddress(String emailAddress); // 3. 네이티브 쿼리 활용
+   *
+   * @Query(value="SELECT * FROM USERS WHERE EMAIL_ADDRESS = ?1", nativeQuery =
+   * true) User findByEmailAddress(String emailAddress);
+   */
 }
--- src/test/java/com/taskagile/TaskAgileApplicationTests.java
+++ src/test/java/com/taskagile/TaskAgileApplicationTests.java
@@ -3,14 +3,17 @@ package com.taskagile;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test") // spring에 테스트중인걸 알림
 public class TaskAgileApplicationTests {
 
 	@Test
 	public void contextLoads() {
+    
 	}
 
 }
